fedf5d8bc21928fa8d71c6c2bf9634df22995646
lwhhhp@gmail.com <lwhhhp@gmail.com>
2021-07-22 03:48:03 +0800

    feat: 开启缓存

--- spring-security-auth/src/main/java/com/lanweihong/security/auth/AuthServerApplication.java
+++ spring-security-auth/src/main/java/com/lanweihong/security/auth/AuthServerApplication.java
@@ -2,12 +2,14 @@ package com.lanweihong.security.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author lanweihong
  * @date 2021/1/13 01:28
  */
 @SpringBootApplication
+@EnableCaching
 public class AuthServerApplication {
 
     public static void main(String[] args) {
